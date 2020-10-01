b959029da96a2dd15a9628569b197dcd5c5b2362
Shubhashish Verma <svashish305@gmail.com>
2020-10-02 04:13:43 +0530

    reset frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,26 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import CreateTodo from "./components/todo/create-todo.component";
-import EditTodo from "./components/todo/edit-todo.component";
-import TodosList from "./components/todo/todos-list.component";
-import DeleteTodo from "./components/todo/delete-todo.component";
-
-import logo from "./logo.svg";
-
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="./" target="_blank">
-              <img src={logo} width="30" height="30" alt="mern-crud" />
-            </a>
-            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
-            <div className="collpase navbar-collapse">
-              <ul className="navbar-nav mr-auto">
-                <li className="navbar-item">
-                  <Link to="/" className="nav-link">Todos</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/create" className="nav-link">Create Todo</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          <br/>
-          <Route path="/" exact component={TodosList} />
-          <Route path="/edit/:id" component={EditTodo} />
-          <Route path="/create" component={CreateTodo} />
-          <Route path="/delete/:id" component={DeleteTodo} />
-        </div>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
-import './styles.less';
-
-import { history } from './history';
-import { userService } from './services/user.service';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-userService.refreshToken().finally(startApp);
-
-function startApp() { 
-    render(
-        <Router history={history}>
-            <App />
-        </Router>,
-        document.getElementById('app')
-    );
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
