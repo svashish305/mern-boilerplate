0b64870627a2f6689232e70294305223f93fc120
Shubhashish Verma <svashish305@gmail.com>
2020-10-02 20:11:45 +0530

    added store to auth

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -15,9 +15,15 @@ class Login extends Component {
     };
   }
 
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
   componentWillReceiveProps(nextProps) {
     if (nextProps.auth.isAuthenticated) {
-      this.props.history.push("/dashboard"); // push user to dashboard when they login
+      this.props.history.push("/dashboard"); 
     }
     if (nextProps.errors) {
       this.setState({
